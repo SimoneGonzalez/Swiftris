1ac71d71a8a9fa56281a7d40c3f1cc31bcbf17e8
Simone Gonzalez <simone.b.gonzalez@gmail.com>
2017-06-29 17:51:43 -0500

    added game start logic

---
+++ Swiftris/Swiftris/Swiftris.swift
@@ -0,0 +1,55 @@
+//
+//  Swiftris.swift
+//  Swiftris
+//
+//  Created by Simone Gonzalez on 6/27/17.
+//  Copyright © 2017 Simone Gonzalez. All rights reserved.
+//
+
+let NumColumns = 10
+
+let NumRows = 20
+
+let StartingColumn = 4
+
+let StartingRow = 0
+
+let PreviewColumn = 12
+
+let PreviewRow = 1
+
+class Swiftris {
+    
+    var blockArray:Array2D<Block>
+    
+    var nextShape:Shape?
+    
+    var fallingShape:Shape?
+    
+    
+    init() {
+        fallingShape = nil
+        
+        nextShape = nil
+
+        blockArray = Array2D<Block>(columns: NumColumns, rows: NumRows)
+    }
+    
+    func beginGame() {
+        
+        if(nextShape == nil) {
+            nextShape = Shape.random(PreviewColumn, startingRow: PreviewRow)
+        }
+    }
+    
+    func newShape() -> (fallingShape:Shape?, nextShape:Shape?) {
+        
+        fallingShape = nextShape
+        
+        nextShape = Shape.random(PreviewColumn, startingRow: PreviewRow)
+        
+        fallingShape?.moveTo(StartingColumn, row: StartingRow)
+        
+        return (fallingShape, nextShape)
+    }
+}
